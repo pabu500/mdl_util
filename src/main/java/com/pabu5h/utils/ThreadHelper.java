0edac1f4d47b883905b50bda1c7222862c5c08da
ggbotbox <oliver.lu@pa.com.sg>
2023-03-05 14:40:42 +0800

    1st commit

---
+++ src/main/java/com/pabu5h/utils/ThreadHelper.java
@@ -0,0 +1,35 @@
+package com.pabu5h.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class ThreadHelper {
+    public static ThreadInfo getTreadInfo(Thread thread) {
+        return ThreadInfo.builder()
+                .id(thread.getId())
+                .name(thread.getName())
+                .status(thread.getState().toString())
+                .priority(thread.getPriority())
+                .isDaemon(thread.isDaemon())
+                .build();
+    }
+
+    public static List<ThreadInfo> getThreadInfoList() {
+        List<ThreadInfo> threadInfoList = new ArrayList<>();
+        Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+        for (Thread t : threadSet) {
+            threadInfoList.add(getTreadInfo(t));
+        }
+        return threadInfoList;
+    }
+
+    public static void printAllThreads() {
+        Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+        System.out.printf("%-34s \t %-15s \t %-15s \t %s\n", "Name", "State", "Priority", "isDaemon");
+        // iterating over the threads to get the names of all the active threads
+        for (Thread t : threadSet) {
+            System.out.printf("%-34s \t %-15s \t %-15d \t %s\n", t.getName(), t.getState(), t.getPriority(), t.isDaemon());
+        }
+    }
+}
---
+++ src/main/java/com/pabu5h/utils/ThreadInfo.java
@@ -0,0 +1,17 @@
+package com.pabu5h.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ThreadInfo {
+    private long id;
+    private String name;
+    private String status;
+    private int priority;
+    private boolean isDaemon;
+
+}
