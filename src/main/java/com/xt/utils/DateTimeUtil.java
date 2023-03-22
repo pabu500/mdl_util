eb65e2748a0747d9f55be16186374d91a0aa79e6
ggbotbox <oliver.lu@pa.com.sg>
2023-03-22 23:37:15 +0800

    change time static to public

--- src/main/java/com/xt/utils/DateTimeUtil.java
+++ src/main/java/com/xt/utils/DateTimeUtil.java
@@ -5,15 +5,28 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
-    final static long fourHours = 14400000L;
-    final static long twoHours = 7200000L;
-    final static long oneHour = 3600000L;
-    final static long halfHour = 1800000L;
-    final static long fifteenMin = 900000L;
-    final static long fiveMin = 300000L;
+    public final static long oneYear = 31536000000L;
+    public final static long halfYear = 15768000000L;
+    public final static long oneMonth = 2592000000L;
+    public final static long halfMonth = 1296000000L;
+    public final static long oneWeek = 604800000L;
+    public final static long halfWeek = 302400000L;
+    public final static long threeDays = 259200000L;
+    public final static long twoDays = 172800000L;
+    public final static long oneDay = 86400000L;
+    public final static long halfDay = 43200000L;
+    public final static long fourHours = 14400000L;
+    public final static long twoHours = 7200000L;
+    public final static long oneHour = 3600000L;
+    public final static long halfHour = 1800000L;
+    public final static long fifteenMin = 900000L;
+    public final static long fiveMin = 300000L;
+    public final static long oneMin = 60000L;
+    public final static long halfMin = 30000L;
+    public final static long tenSec = 10000L;
 
-    private final static String desTimeStampFormat = "yyyy-MM-dd HH:mm:ss";
-    private final static String desTimeStampFormatIncludingMs = "yyyy-MM-dd HH:mm:ss.SSS";
+    public  final static String desTimeStampFormat = "yyyy-MM-dd HH:mm:ss";
+    public  final static String desTimeStampFormatIncludingMs = "yyyy-MM-dd HH:mm:ss.SSS";
 
     public final static SimpleDateFormat sdf = new SimpleDateFormat(desTimeStampFormat);
     public final static DateTimeFormatter formatter = DateTimeFormatter.ofPattern(desTimeStampFormat);
