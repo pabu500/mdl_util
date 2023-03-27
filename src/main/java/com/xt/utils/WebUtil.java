ece812b46ba7168c9478b04feb33ad961b94144f
Oliver Lu <pa596bu500@gmail.com>
2023-03-27 01:09:38 +0000

    add WebUtil

---
+++ src/main/java/com/xt/utils/WebUtil.java
@@ -0,0 +1,15 @@
+package com.xt.utils;
+
+//Spring Boot 3.0+
+//import jakarta.servlet.http.HttpServletRequest;
+//Spring Boot 2.5+
+import javax.servlet.http.HttpServletRequest;
+
+public class WebUtil {
+
+    //get the site URL from HttpServletRequest
+    public static String getSiteURL(HttpServletRequest request) {
+        String siteURL = request.getRequestURL().toString();
+        return siteURL.replace(request.getServletPath(), "");
+    }
+}
