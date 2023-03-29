52feeb61f42d7cfd6ef5ec16d0e82a16b90c7877
ggbotbox <oliver.lu@pa.com.sg>
2023-03-29 22:33:55 +0800

    add obj to Float, Long, Integer

--- src/main/java/com/xt/utils/MathUtil.java
+++ src/main/java/com/xt/utils/MathUtil.java
@@ -10,4 +10,31 @@ public class MathUtil {
         }
         return ((Number) obj).doubleValue();
     }
+    public static Long ObjToLong(Object obj) {
+        if (obj == null) {
+            return null;
+        }
+        if (obj instanceof String) {
+            return Long.parseLong(obj.toString());
+        }
+        return ((Number) obj).longValue();
+    }
+    public static Float ObjToFloat(Object obj) {
+        if (obj == null) {
+            return null;
+        }
+        if (obj instanceof String) {
+            return Float.parseFloat(obj.toString());
+        }
+        return ((Number) obj).floatValue();
+    }
+    public static Integer ObjToInteger(Object obj) {
+        if (obj == null) {
+            return null;
+        }
+        if (obj instanceof String) {
+            return Integer.parseInt(obj.toString());
+        }
+        return ((Number) obj).intValue();
+    }
 }
