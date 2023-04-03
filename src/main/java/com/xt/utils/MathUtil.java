bf35b7ace02af191a13d7a3bd713f04515e93770
ggbotbox <oliver.lu@pa.com.sg>
2023-04-03 23:16:19 +0800

    add convert to timestamp

--- src/main/java/com/xt/utils/MathUtil.java
+++ src/main/java/com/xt/utils/MathUtil.java
@@ -1,5 +1,9 @@
 package com.xt.utils;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MathUtil {
     public static Double ObjToDouble(Object obj) {
         if (obj == null) {
@@ -36,5 +40,24 @@ public class MathUtil {
             return Integer.parseInt(obj.toString());
         }
         return ((Number) obj).intValue();
+    }
+
+    public static Long findDominantLong(List<Double> numbers) {
+        Map<Long, Integer> frequencyMap = new HashMap<>();
+        long dominantLong = Math.round(numbers.get(0));
+        int dominantFrequency = 1;
+
+        for (double number : numbers) {
+            Long candidate = Math.round(number);
+            int count = frequencyMap.getOrDefault(candidate, 0) + 1;
+            frequencyMap.put(candidate, count);
+
+            if (count > dominantFrequency) {
+                dominantLong = candidate;
+                dominantFrequency = count;
+            }
+        }
+
+        return dominantLong;
     }
 }
