299b6160e4b40e4c8dd02d266f3e981b16af32f0
Oliver Lu <pa596bu500@gmail.com>
2023-05-22 04:06:59 +0000

    add currency to provider

--- src/main/java/com/xt/utils/XtCurrencyUnitDataProvider.java
+++ src/main/java/com/xt/utils/XtCurrencyUnitDataProvider.java
@@ -2,11 +2,13 @@ package com.xt.utils;
 
 import org.joda.money.CurrencyUnitDataProvider;
 
-public class SgdCurrencyUnitDataProvider extends CurrencyUnitDataProvider {
+public class XtCurrencyUnitDataProvider extends CurrencyUnitDataProvider {
     @Override
     protected void registerCurrencies() throws Exception {
         // The parameters are: code, numeric code, decimal places
         //ISO 4217 for SGD is 702
         registerCurrency("SGD", 702, 2);
+        //currency code for XBT is not standard, use 2009 for now
+        registerCurrency("XBT", 2009, 8);
     }
 }
