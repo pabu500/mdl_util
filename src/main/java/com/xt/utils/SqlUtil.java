a9698af17a827e2d2a78d9cd1b7b81bb2f435fad
ggbotbox <oliver.lu@pa.com.sg>
2023-06-19 21:56:38 +0800

    add makeSelectLikeSql

--- src/main/java/com/xt/utils/SqlUtil.java
+++ src/main/java/com/xt/utils/SqlUtil.java
@@ -72,6 +72,75 @@ public class SqlUtil {
         return Map.of("sql", sql.toString());
     }
 
+    public static Map<String, String> makeSelectLikeSql(Map<String, Object> sqlMap) {
+        StringBuilder sql = new StringBuilder();
+        sql.append("SELECT ");
+        if(sqlMap.get("select") != null) {
+            sql.append(sqlMap.get("select"));
+        } else {
+            sql.append("*");
+        }
+
+        sql.append(" FROM ");
+        if(sqlMap.get("from") != null) {
+            sql.append(sqlMap.get("from"));
+        } else {
+            return Map.of("error", "Missing table name");
+        }
+
+        String targetConstraint = "";
+        if(sqlMap.get("target_key") != null) {
+            targetConstraint = sqlMap.get("target_key") + " like '%" + sqlMap.get("target_value") + "%'";
+        }else{
+            //multiple target
+            if(sqlMap.get("targets") != null){
+                if(sqlMap.get("targets") instanceof Map<?,?>){
+                    Map<String, String> targets = (Map<String, String>) sqlMap.get("targets");
+                    if(!targets.keySet().isEmpty()) {
+                        for (String key : targets.keySet()) {
+                            targetConstraint += key + " = '" + targets.get(key) + "' AND ";
+                        }
+                        targetConstraint = targetConstraint.substring(0, targetConstraint.length() - 5);
+                    }
+                }
+            }
+        }
+
+        String timeConstraint = "";
+        if(sqlMap.get("time_key") != null) {
+            if(sqlMap.get("start_datetime")!=null){
+                timeConstraint = sqlMap.get("time_key") + " >= '" + sqlMap.get("start_datetime");
+            }
+            if(sqlMap.get("end_datetime")!=null){
+                if(timeConstraint.equals("")){
+                    timeConstraint = sqlMap.get("time_key") + " <= '" + sqlMap.get("end_datetime");
+                }else{
+                    timeConstraint += "' AND " + sqlMap.get("time_key") + " <= '" + sqlMap.get("end_datetime");
+                }
+            }
+//            timeConstraint = sqlMap.get("time_key") + " >= '" + sqlMap.get("start_datetime") + "' AND " + sqlMap.get("time_key") + " <= '" + sqlMap.get("end_datetime") + "'";
+        }
+
+        if(!targetConstraint.equals("") && !timeConstraint.equals("")) {
+            sql.append(" WHERE ").append(targetConstraint).append(" AND ").append(timeConstraint);
+        } else if(!targetConstraint.equals("")) {
+            sql.append(" WHERE ").append(targetConstraint);
+        } else if(!timeConstraint.equals("")) {
+            sql.append(" WHERE ").append(timeConstraint);
+        }
+
+        if(sqlMap.get("time_key")!=null){
+            sql.append(" ORDER BY ").append(sqlMap.get("time_key")).append(" DESC");
+        }
+
+        String limit = sqlMap.get("limit").toString();
+        if(limit != null) {
+            sql.append(" LIMIT ").append(limit);
+        }
+
+        return Map.of("sql", sql.toString());
+    }
+
     public static Map<String, String> makeInsertSql(Map<String, Object> sqlMap) {
         StringBuilder sql = new StringBuilder();
         sql.append("INSERT INTO ");
