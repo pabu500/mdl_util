59cf54187e5d8b1e49d4607602d2e35ee6d7b500
Oliver Lu <pa596bu500@gmail.com>
2023-07-10 01:08:19 +0000

    misc update

--- src/main/java/com/xt/utils/SqlUtil.java
+++ src/main/java/com/xt/utils/SqlUtil.java
@@ -19,9 +19,9 @@ public class SqlUtil {
             return Map.of("error", "Missing table name");
         }
 
-        String targetConstraint = "";
+        StringBuilder targetConstraint = new StringBuilder();
         if(sqlMap.get("target_key") != null) {
-            targetConstraint = sqlMap.get("target_key") + " = '" + sqlMap.get("target_value") + "'";
+            targetConstraint = new StringBuilder(sqlMap.get("target_key") + " = '" + sqlMap.get("target_value") + "'");
         }else{
             //multiple target
             if(sqlMap.get("targets") != null){
@@ -29,9 +29,9 @@ public class SqlUtil {
                     Map<String, String> targets = (Map<String, String>) sqlMap.get("targets");
                     if(!targets.keySet().isEmpty()) {
                         for (String key : targets.keySet()) {
-                            targetConstraint += key + " = '" + targets.get(key) + "' AND ";
+                            targetConstraint.append(key).append(" = '").append(targets.get(key)).append("' AND ");
                         }
-                        targetConstraint = targetConstraint.substring(0, targetConstraint.length() - 5);
+                        targetConstraint = new StringBuilder(targetConstraint.substring(0, targetConstraint.length() - 5));
                     }
                 }
             }
@@ -52,9 +52,9 @@ public class SqlUtil {
 //            timeConstraint = sqlMap.get("time_key") + " >= '" + sqlMap.get("start_datetime") + "' AND " + sqlMap.get("time_key") + " <= '" + sqlMap.get("end_datetime") + "'";
         }
 
-        if(!targetConstraint.equals("") && !timeConstraint.equals("")) {
+        if(!targetConstraint.toString().equals("") && !timeConstraint.equals("")) {
             sql.append(" WHERE ").append(targetConstraint).append(" AND ").append(timeConstraint);
-        } else if(!targetConstraint.equals("")) {
+        } else if(!targetConstraint.toString().equals("")) {
             sql.append(" WHERE ").append(targetConstraint);
         } else if(!timeConstraint.equals("")) {
             sql.append(" WHERE ").append(timeConstraint);
