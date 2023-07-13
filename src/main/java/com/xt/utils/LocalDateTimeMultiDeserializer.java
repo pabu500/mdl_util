c5401b7c9802e9c5c4aa4f1ab59527e30b759b1f
Oliver Lu <pa596bu500@gmail.com>
2023-07-13 08:28:55 +0000

    add LocalDateTimeMultiDeserializer

---
+++ src/main/java/com/xt/utils/LocalDateTimeMultiDeserializer.java
@@ -0,0 +1,23 @@
+package com.xt.utils;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeMultiDeserializer extends JsonDeserializer<LocalDateTime>{
+    @Override
+    public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, IOException {
+        String timestampString = jsonParser.readValueAs(String.class);
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss[.SSS]");
+            return LocalDateTime.parse(timestampString, formatter);
+        } catch (Exception e) {
+            // If the custom pattern parsing fails, try ISO_LOCAL_DATE_TIME
+            return LocalDateTime.parse(timestampString, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        }
+    }
+}
