458c72975d808102d52ddaf3d65eff58b3746541
ggbotbox <oliver.lu@pa.com.sg>
2024-03-17 12:39:19 +0800

    misc fix

--- src/main/java/com/xt/utils/SqlUtil.java
+++ src/main/java/com/xt/utils/SqlUtil.java
@@ -347,13 +347,13 @@ public class SqlUtil {
         String timeConstraint = "";
         if(sqlMap.get("time_key") != null) {
             if(sqlMap.get("start_datetime")!=null){
-                timeConstraint = sqlMap.get("time_key") + " >= '" + sqlMap.get("start_datetime");
+                timeConstraint = sqlMap.get("time_key") + " >= '" + sqlMap.get("start_datetime") + "'";
             }
             if(sqlMap.get("end_datetime")!=null){
                 if(timeConstraint.isEmpty()){
-                    timeConstraint = sqlMap.get("time_key") + " <= '" + sqlMap.get("end_datetime");
+                    timeConstraint = sqlMap.get("time_key") + " <= '" + sqlMap.get("end_datetime") + "'";
                 }else{
-                    timeConstraint += "' AND " + sqlMap.get("time_key") + " <= '" + sqlMap.get("end_datetime");
+                    timeConstraint += " AND " + sqlMap.get("time_key") + " <= '" + sqlMap.get("end_datetime") + "'";
                 }
             }
 //            timeConstraint = sqlMap.get("time_key") + " >= '" + sqlMap.get("start_datetime") + "' AND " + sqlMap.get("time_key") + " <= '" + sqlMap.get("end_datetime") + "'";
