179f40310eca2faedf969c9a42d8aa17309b0a18
Saw Kean Chuan <pa622bu500@gmail.com>
2026-09-24 09:07:49 +0800

    LocalDateTimeMultiDeserializer Update

--- src/main/java/org/pabuff/utils/LocalDateTimeMultiDeserializer.java
+++ src/main/java/org/pabuff/utils/LocalDateTimeMultiDeserializer.java
@@ -7,13 +7,19 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 
 public class LocalDateTimeMultiDeserializer extends JsonDeserializer<LocalDateTime>{
     @Override
     public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, IOException {
         String timestampString = jsonParser.readValueAs(String.class);
         try {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss[.SSS]");
+            // .1, .12, .123 decimal seconds are supported
+            DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                    .appendPattern("yyyy-MM-dd HH:mm:ss")
+                    .appendFraction(ChronoField.NANO_OF_SECOND, 0, 3, true)
+                    .toFormatter();
             return LocalDateTime.parse(timestampString, formatter);
         } catch (Exception e) {
             // If the custom pattern parsing fails, try ISO_LOCAL_DATE_TIME
